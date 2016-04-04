b3db334ec772a205b28587061c4806c360ffc0e8
Jonathon Nordquist <cugamer@gmail.com>
2016-04-03 22:40:02 -0500

    Add zoom in and out behaviors, NikonCamera class

--- Cameras.rb
+++ Cameras.rb
@@ -4,11 +4,11 @@ class Camera
   end
 
   def zoomIn
-    zoomInBehavior.zoomInBehavior
+    return "#{@description}  #{@zoomInBehavior.zoomInBehavior}"
   end
 
   def zoomOut
-    zoomOutBehavior.zoomOutBehavior
+    return "#{@description} #{@zoomOutBehavior.zoomOutBehavior}"
   end
 end
 
@@ -16,6 +16,8 @@ class DisposableCamera < Camera
   def initialize
     @description = "Cheep disposable camera"
     @takePictureBehavior = TakeStillImage.new
+    @zoomInBehavior = NoZoomIn.new
+    @zoomOutBehavior = NoZoomOut.new
   end
 end
 
@@ -23,6 +25,19 @@ class VideoCamera < Camera
   def initialize
     @description = "High def video camera"
     @takePictureBehavior = TakeVideo.new
+    @zoomInBehavior = ZoomIn.new
+    @zoomOutBehavior = ZoomOut.new
+  end
+end
+
+class NikonCamera < Camera
+  attr_writer :takePictureBehavior
+
+  def initialize
+    @description = "Quality nikon camera"
+    @takePictureBehavior = TakeStillImage.new
+    @zoomInBehavior = ZoomIn.new
+    @zoomOutBehavior = ZoomOut.new
   end
 end
 
@@ -38,8 +53,47 @@ class TakeVideo
   end
 end
 
+class ZoomIn
+  def zoomInBehavior
+    return "zooms in"
+  end
+end
+
+class NoZoomIn
+  def zoomInBehavior
+    "unable to zoom in"
+  end
+end
+
+class ZoomOut
+  def zoomOutBehavior
+    return "zooms out"
+  end
+end
+
+class NoZoomOut
+  def zoomOutBehavior
+    return "unable to zoom out"
+  end
+end
+
 disposable = DisposableCamera.new
 video = VideoCamera.new
+nikon = NikonCamera.new
 
 p disposable.takePicture
 p video.takePicture
+p nikon.takePicture
+
+p disposable.zoomIn
+p video.zoomIn
+p nikon.zoomIn
+
+p disposable.zoomOut
+p video.zoomOut
+p nikon.zoomOut
+
+# The Nikon camera dynamically gets a new behavior and is able to use
+# it at runtime.
+nikon.takePictureBehavior = TakeVideo.new
+p nikon.takePicture
